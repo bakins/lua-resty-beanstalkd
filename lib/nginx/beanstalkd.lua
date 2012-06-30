01f9af4dca6a3afb44adb4b03c7aeaa8c50f969f
Brian Akins <brian@akins.org>
2012-06-30 09:08:10 -0400

    get put working. we can worry about abstractions later

--- lib/nginx/beanstalkd.lua
+++ lib/nginx/beanstalkd.lua
@@ -5,7 +5,7 @@ _M._VERSION = '0.0.1'
 local mt = {}
 
 local tcp = ngx.socket.tcp
-local find = string.find
+local match, concat = string.match, table.concat
 
 function _M.new(host, port, options)
     options = options or {}
@@ -35,59 +35,37 @@ function mt.close(self, really_close)
     if really_close then
         return self.sock:close()
     else
-        return self.sock:setkeepalive(self.keepalive_timeout, self.keepalive_pool_size)
+        --   return self.sock:setkeepalive(self.keepalive_timeout, self.keepalive_pool_size)
     end
 end
 
 -- interface is based on https://github.com/kr/beanstalk-client-ruby
 
--- "private" helper
--- expects table of command and a table of expected 
--- returns (patterns)
--- format is expected format - mutli line, etc
-local function interact(self, cmd, expect, format)
-    format = format or "line"
-    local sock = self.sock
-    
-    local bytes, err = sock:send(cmd)
-    if not bytes then
-        return nil, err
-    end
-    
-    -- all responses have at least one line
-    local line, err = sock:receive()
-    if not line then
-        return nil, err
-    end
-    
-    if "line" == format then
-        for _,pattern in ipairs(expect) do
-            if find(line, pattern) then
-                return true, line
-            end
-        end
-        return false, line
-    end
-    
-    return nil, "unknown format"
-end
-
 function mt.put(self, body, pri, delay, ttr)
     pri = pri or 65536
     delay = delay or 0
     ttr = ttr or 120
-
-    local rc, line = 
-        interact(self, 
-                 {"put", pri, delay, ttr, #body, "\r\n", body, "\r\n" }, 
-                 { "^INSERTED", "^BURIED"},
-                 "line")
+    local sock = self.sock
     
-    if rc and line == "INSERTED" then
-        return true
-    else
-        return rc, line
-    end
-end
+    local cmd =  {"put ", pri, " ", delay, " ", ttr, " ", #body, "\r\n", body, "\r\n" }
+    local bytes, err = sock:send(cmd)
+     if not bytes then
+        return nil, err
+     end
+     local line, err = sock:receive()
+     if not line then
+         return nil, err
+     end
+     
+     local status, id = match(line, '(%u+)%s+(%d+)$')
+     if not status then
+         return nil, nil, line
+     end
+     if "INSERTED" == status then
+         return true, id, status
+     else
+         return false, id, status
+     end
+ end
 
 return _M
